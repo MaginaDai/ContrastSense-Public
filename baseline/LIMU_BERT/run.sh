3f7108bf8189a37c3e64163786a29a50dae217a4
gaole_WANDS_3060 <gaole001@e.ntu.edu.sg>
2022-10-05 10:46:45 +0800

    change .gitignore for LIMU

--- baseline/LIMU_BERT/run.sh
+++ baseline/LIMU_BERT/run.sh
@@ -15,33 +15,39 @@
 
 # wait
 
-# python classifier_bert.py v1_v2 20_120 -p HHAR -f CrossVal_HHAR -name 'UCI' -s limu_gru_UCI_1 &
-# python classifier_bert.py v1_v2 20_120 -p HHAR -f CrossVal_HHAR -name 'MotionSense' -s limu_gru_MotionSense_1 &
-# python classifier_bert.py v1_v2 20_120 -p HHAR -f CrossVal_HHAR -name 'Shoaib' -s limu_gru_Shoaib_1 &
-# python classifier_bert.py v1_v2 50_200 -p 'HHAR 50_200' -f HHAR_50_200 -name 'HHAR 50_200' -s limu_gru_hhar_50_200
+name='50_200'
+# dataset='ICHAR'
+# python -u pretrain.py v1 ${name} -name ${dataset} -g 0 -s "${dataset}_${name}" 
+# python classifier_bert.py v1_v2 ${name} -p ${dataset} -f "${dataset}_${name}" -name ${dataset} -s "${dataset}_${name}"
+
+python classifier_bert.py v1_v2 ${name} -p MotionSense -f "MotionSense_${name}" -name 'HASC' -s limu_gru_MotionSense &
+python classifier_bert.py v1_v2 ${name} -p Shoaib -f "Shoaib_${name}" -name 'HASC' -s limu_gru_Shoaib &
+python classifier_bert.py v1_v2 ${name} -p HHAR -f "HHAR_${name}" -name 'HASC' -s limu_gru_HHAR &
+python classifier_bert.py v1_v2 ${name} -p HASC -f "HASC_${name}" -name 'HASC' -s limu_gru_HASC
+
+wait
+
+python classifier_bert.py v1_v2 ${name} -p MotionSense -f "MotionSense_${name}" -name 'HHAR' -s limu_gru_MotionSense &
+python classifier_bert.py v1_v2 ${name} -p Shoaib -f "Shoaib_${name}" -name 'HHAR' -s limu_gru_Shoaib &
+python classifier_bert.py v1_v2 ${name} -p HHAR -f "HHAR_${name}" -name 'HHAR' -s limu_gru_HHAR &
+python classifier_bert.py v1_v2 ${name} -p HASC -f "HASC_${name}" -name 'HHAR' -s limu_gru_HASC
+
+wait
+
+python classifier_bert.py v1_v2 ${name} -p MotionSense -f "MotionSense_${name}" -name 'MotionSense' -s limu_gru_MotionSense &
+python classifier_bert.py v1_v2 ${name} -p Shoaib -f "Shoaib_${name}" -name 'MotionSense' -s limu_gru_Shoaib &
+python classifier_bert.py v1_v2 ${name} -p HHAR -f "HHAR_${name}" -name 'MotionSense' -s limu_gru_HHAR &
+python classifier_bert.py v1_v2 ${name} -p HASC -f "HASC_${name}" -name 'MotionSense' -s limu_gru_HASC
+
+wait
+
+python classifier_bert.py v1_v2 ${name} -p MotionSense -f "MotionSense_${name}" -name 'Shoaib' -s limu_gru_MotionSense &
+python classifier_bert.py v1_v2 ${name} -p Shoaib -f "Shoaib_${name}" -name 'Shoaib' -s limu_gru_Shoaib &
+python classifier_bert.py v1_v2 ${name} -p HHAR -f "HHAR_${name}" -name 'Shoaib' -s limu_gru_HHAR &
+python classifier_bert.py v1_v2 ${name} -p HASC -f "HASC_${name}" -name 'Shoaib' -s limu_gru_HASC
 
 
-# wait
 
-# python classifier_bert.py v1_v2 20_120 -f MotionSense -name 'UCI' -s limu_gru_MotionSense &
-# python classifier_bert.py v1_v2 20_120 -f Shoaib -name 'UCI' -s limu_gru_Shoaib &
-# python classifier_bert.py v1_v2 20_120 -f HHAR -name 'UCI' -s limu_gru_hhar &
-# python classifier_bert.py v1_v2 20_120 -f UCI -name 'UCI' -s limu_gru_UCI 
-
-# wait
-
-# python classifier_bert.py v1_v2 20_120 -f MotionSense -name 'Shoaib' -s limu_gru_MotionSense &
-# python classifier_bert.py v1_v2 20_120 -f Shoaib -name 'Shoaib' -s limu_gru_Shoaib &
-# python classifier_bert.py v1_v2 20_120 -f HHAR -name 'Shoaib' -s limu_gru_hhar &
-# python classifier_bert.py v1_v2 20_120 -f UCI -name 'Shoaib' -s limu_gru_UCI
-
-
-# wait
-
-# python classifier_bert.py v1_v2 20_120 -f MotionSense -name 'MotionSense' -s limu_gru_MotionSense &
-# python classifier_bert.py v1_v2 20_120 -f Shoaib -name 'MotionSense' -s limu_gru_Shoaib &
-# python classifier_bert.py v1_v2 20_120 -f HHAR -name 'MotionSense' -s limu_gru_hhar &
-# python classifier_bert.py v1_v2 20_120 -f UCI -name 'MotionSense' -s limu_gru_UCI &
 
 
 # for num in 2 3 4 5 6
@@ -50,10 +56,7 @@
 #     python classifier_bert.py v1_v2 "50_200_test_${num}" -p "HHAR" -f "HHAR_50_200_test_${num}" -name 'HHAR' -s "limu_gru_HHAR_50_200_test_${num}"
 # done
 
-name='50_200'
-dataset='ICHAR'
-# python -u pretrain.py v1 ${name} -name ${dataset} -g 0 -s "${dataset}_${name}" 
-python classifier_bert.py v1_v2 ${name} -p ${dataset} -f "${dataset}_${name}" -name ${dataset} -s "${dataset}_${name}"
+
 
 # dataset='Shoaib'
 # python -u pretrain.py v1 ${name} -name ${dataset} -g 0 -s "${dataset}_${name}" 
--- run.sh
+++ run.sh
@@ -20,13 +20,13 @@
 # done
 
 
-a=0.3
-for e in 2000
-do 
-    name="DAL"
-    namea="CE_2000_cos_trans"
-    nameb="contrast_wo"
-    namec="no"
+# a=0.3
+# for e in 2000
+# do 
+#     name="DAL"
+#     namea="CE_2000_cos_trans"
+#     nameb="contrast_wo"
+#     namec="no"
     # python main.py -g 0 -e ${e}  -label_type 1 -slr ${a} -CE True -name MotionSense --store "${name}_MotionSense" &
     # python main.py -g 0 -e ${e}  -label_type 1 -slr ${a} -CE True -name HASC --store "${name}_HASC" &
     # python main.py -g 1 -e ${e}  -label_type 1 -slr ${a} -CE True -name HHAR --store "${name}_HHAR" &
@@ -34,10 +34,10 @@ do
 
     # wait
 
-    python main_transfer.py -g 0 -ft True -lr 0.0005 -name HASC --pretrained "${name}_HASC" --store "${name}_1_HASC" &
-    python main_transfer.py -g 0 -ft True -lr 0.0005 -name HHAR --pretrained "${name}_HHAR" --store "${name}_1_HHAR" &
-    python main_transfer.py -g 1 -ft True -lr 0.0005 -name Shoaib --pretrained "${name}_Shoaib" --store "${name}_1_Shoaib" &
-    python main_transfer.py -g 1 -ft True -lr 0.0005 -name MotionSense  --pretrained "${name}_MotionSense" --store "${name}_1_MotionSense" 
+    # python main_transfer.py -g 0 -ft True -lr 0.0005 -name HASC --pretrained "${name}_HASC" --store "${name}_1_HASC" &
+    # python main_transfer.py -g 0 -ft True -lr 0.0005 -name HHAR --pretrained "${name}_HHAR" --store "${name}_1_HHAR" &
+    # python main_transfer.py -g 1 -ft True -lr 0.0005 -name Shoaib --pretrained "${name}_Shoaib" --store "${name}_1_Shoaib" &
+    # python main_transfer.py -g 1 -ft True -lr 0.0005 -name MotionSense  --pretrained "${name}_MotionSense" --store "${name}_1_MotionSense" 
 
     # wait
 
@@ -66,7 +66,31 @@ do
     # python main_transfer.py -g 1 -ft True -lr 0.0005 -name MotionSense  --pretrained "${namec}_MotionSense"
 
 
-done
+# done
 # name="DAL"
 # python main.py -g 1 -name HHAR --store "${name}_HHAR" -DAL True
 # python main_transfer.py -g 1 -lr 0.0005 -name HHAR  --pretrained "${name}_HHAR"
+
+
+######### transfer learning ##########
+
+name=contrast_w
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HASC --pretrained "${name}_HHAR" &
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HHAR --pretrained "${name}_HHAR" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name Shoaib --pretrained "${name}_HHAR" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name MotionSense  --pretrained "${name}_HHAR"
+wait
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HASC --pretrained "${name}_HASC" &
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HHAR --pretrained "${name}_HASC" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name Shoaib --pretrained "${name}_HASC" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name MotionSense  --pretrained "${name}_HASC"
+wait
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HASC --pretrained "${name}_Shoaib" &
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HHAR --pretrained "${name}_Shoaib" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name Shoaib --pretrained "${name}_Shoaib" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name MotionSense  --pretrained "${name}_Shoaib"
+wait
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HASC --pretrained "${name}_MotionSense" &
+python main_transfer.py -g 0 -ft True -lr 0.0005 -name HHAR --pretrained "${name}_MotionSense" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name Shoaib --pretrained "${name}_MotionSense" &
+python main_transfer.py -g 1 -ft True -lr 0.0005 -name MotionSense  --pretrained "${name}_MotionSense"
